7383faa2f2ba2518ec519accc19a19db2334e950
CarbonTornado <talkinator@protonmail.com>
2017-12-02 23:04:27 +0100

    Initial commit

---
+++ app/src/main/java/app/de/sv_gss/navdrawer/CustomAdapter.java
@@ -0,0 +1,75 @@
+package app.de.sv_gss.navdrawer;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private ArrayList<String> name;
+    private ArrayList<String> beschreibung;
+    private ArrayList<String> leiter;
+    private ArrayList<String> termine;
+    private Context context;
+
+    protected CustomAdapter(Context context, ArrayList<String> name, ArrayList<String> beschreibung, ArrayList<String> leiter, ArrayList<String> termine) {
+        this.context = context;
+        this.name = name;
+        this.beschreibung = beschreibung;
+        this.leiter = leiter;
+        this.termine = termine;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // infalte the item Layout
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rowlayout, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        // set the data in items
+        holder.name.setText(name.get(position));
+        holder.email.setText(beschreibung.get(position));
+        holder.leiter.setText(leiter.get(position));
+        holder.mobileNo.setText(termine.get(position));
+        // implement setOnClickListener event on item view.
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // display a toast with person name on item click
+                Toast.makeText(context, name.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return name.size();
+    }
+
+    protected class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name, email, mobileNo, leiter;// init the item view's
+
+        protected MyViewHolder(View itemView) {
+            super(itemView);
+
+            // get the reference of item view's
+            name = itemView.findViewById(R.id.name);
+            email = itemView.findViewById(R.id.email);
+            leiter = itemView.findViewById(R.id.leiter);
+            mobileNo = itemView.findViewById(R.id.mobileNo);
+
+        }
+    }
+}
---
+++ app/src/main/java/app/de/sv_gss/navdrawer/MainActivity.java
@@ -0,0 +1,321 @@
+package app.de.sv_gss.navdrawer;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    // ArrayList for person names, email Id's and mobile numbers
+    ArrayList<String> name = new ArrayList<>();
+    ArrayList<String> beschreibung = new ArrayList<>();
+    ArrayList<String> leiter = new ArrayList<>();
+    ArrayList<String> termine = new ArrayList<>();
+
+    RecyclerView recyclerView;
+
+    boolean connected = false;
+    boolean geladen = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        // we are connected to a network
+        connected = (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED) ||
+                (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED);
+
+        // get the reference of RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        // set a LinearLayoutManager with default vertical orientation
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+    private void getJSON(final String urlWebService) {
+        /*
+        * As fetching the json string is a network operation
+        * And we cannot perform a network operation in main thread
+        * so we need an AsyncTask
+        * The constrains defined here are
+        * Void -> We are not passing anything
+        * Void -> Nothing at progress update as well
+        * String -> After completion it should return a string and it will be the json string
+        * */
+
+        //Toast.makeText(getApplicationContext(), "getJSON", Toast.LENGTH_SHORT).show();
+        @SuppressLint("StaticFieldLeak")
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                if(s != null && !s.trim().isEmpty()){
+                    writeToFile(s,getApplicationContext());
+                }
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void loadIntoListView(String json) throws JSONException {
+        //if(json != null && !json.trim().isEmpty()) {
+        //creating a json array from the json string
+        JSONArray jsonArray = new JSONArray(json);
+
+        //creating a string array for listview
+        String[] ids = new String[jsonArray.length()];
+
+        //looping through all the elements in json array
+        for (int i = 0; i < jsonArray.length(); i++) {
+
+            JSONObject userDetail = jsonArray.getJSONObject(i);
+            // fetch email and name and store it in arraylist
+            name.add(userDetail.getString("name"));
+            beschreibung.add(userDetail.getString("beschreibung"));
+            // fetch mobile number and store it in arraylist
+            leiter.add(userDetail.getString("leiter"));
+            termine.add(userDetail.getString("datum"));
+
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, name, beschreibung,leiter , termine);
+        recyclerView.setAdapter(customAdapter); // set the Adapter to RecyclerView
+        geladen=true;
+
+        NotificationManager notif=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notify=new Notification.Builder
+                (getApplicationContext()).setContentTitle("Aktualisiert").setContentText("Deine Webdaten wurden Aktualisiert.").
+                setContentTitle("Aktualisiert").setSmallIcon(R.drawable.ic_menu_about).build();
+
+        notify.flags |= Notification.FLAG_AUTO_CANCEL;
+        notif.notify(0, notify);
+        //}
+
+
+    }
+
+    private void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("json.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+
+        }
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("json.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the home action
+
+        } else if (id == R.id.nav_gallery) {
+            if(!geladen) {
+                getJSON("https://sv-gss.de/app/arbeitsgruppe.php?token=1303");
+                String s = readFromFile(getApplicationContext());
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        }/* else if (id == R.id.nav_send) {
+
+        }*/
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
